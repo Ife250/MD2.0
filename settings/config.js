d3d4a8f8963f1bbacf90f4b811b58052682585f4
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-06-17 22:40:40 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['265889976968']
+global.premium = ['265889976968']
+global.packname = 'kingsley'
+global.author = 'kingsley Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
